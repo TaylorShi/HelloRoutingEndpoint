45b7d917291ad1e051f8f3b2a6af390f4682e4ae
YongAn <414675056@qq.com>
2022-10-14 16:10:05 +0800

    1

---
+++ demoForRouting31/Controllers/OrderController.cs
@@ -0,0 +1,99 @@
+﻿using demoForRouting31.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace demoForRouting31.Controllers
+{
+    /// <summary>
+    /// 订单控制器
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// 订单是否存在
+        /// </summary>
+        /// <remarks>
+        /// 请求示例:
+        ///
+        ///     GET /api/order/OrderExist/123
+        ///     
+        /// </remarks>
+        /// <param name="id">必须可以转为Long</param>
+        /// <returns></returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
+        [HttpGet("{id:IsLong}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public bool OrderExist([FromRoute]object id)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 订单最大值
+        /// </summary>
+        /// <param name="id">最大值20</param>
+        /// <param name="linkGenerator"></param>
+        /// <returns></returns>
+        [HttpGet("{id:max(20)}")]
+        public bool OrderMax(long id, [FromServices]LinkGenerator linkGenerator)
+        {
+            // 获取请求路径
+            var actionPath = linkGenerator.GetPathByAction(HttpContext,
+                action: "OrderRequest",
+                controller: "Order",
+                values: new { name = "abc" });
+
+            Console.WriteLine($"ActionPath: {actionPath}");
+
+            // 获取完整URL
+            var actionUri = linkGenerator.GetUriByAction(HttpContext,
+                action: "OrderRequest",
+                controller: "Order",
+                values: new { name = "abc" });
+
+            Console.WriteLine($"ActionUrl: {actionUri}");
+
+            return true;
+        }
+
+        /// <summary>
+        /// 订单请求
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{name:required}")]
+        [Obsolete]
+        public bool OrderRequest(string name)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        /// <param name="number">必须是三个数字</param>
+        /// <returns></returns>
+        [HttpGet("{number:regex(^\\d{{3}}$)}")]
+        public bool OrderNumber(string number)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 获取订单项
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:IsLong}")]
+        public OrderItem GetOrderItem(long id)
+        {
+            return new OrderItem { Id = id };
+        }
+    }
+}
--- demoForRouting31/Startup.cs
+++ demoForRouting31/Startup.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using demoForRouting31.Consraints;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace demoForRouting31
 {
@@ -29,12 +31,43 @@ namespace demoForRouting31
         {
             services.AddControllers();
 
+            //services.AddSwaggerGen(swaggerGenOptions =>
+            //{
+            //    swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Tesla API", Version = "v1"});
+            //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            //    swaggerGenOptions.IncludeXmlComments(xmlPath);
+            //});
+
             services.AddSwaggerGen(swaggerGenOptions =>
             {
-                swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Tesla API", Version = "v1"});
+                swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
+                { 
+                    Version = "v1",
+                    Title = "Tesla API",
+                    Description = "这是一个示例演示",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Taylor Shi",
+                        Email = string.Empty,
+                        Url = new Uri("https://www.cnblogs.com/taylorshi"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 swaggerGenOptions.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddRouting(routeOptions =>
+            {
+                routeOptions.ConstraintMap.Add("IsLong", typeof(TeslaRouteConsraint));
             });
         }
 
@@ -48,10 +81,21 @@ namespace demoForRouting31
 
             app.UseSwagger();
 
+            //app.UseSwagger(swaggerOptions =>
+            //{
+            //    swaggerOptions.SerializeAsV2 = true;
+            //});
+
             app.UseSwaggerUI(swaggerUIOptions =>
             {
-                swaggerUIOptions.SwaggerEndpoint("v1/swagger.json", "Tesla API v1");
+                swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Tesla API v1");
             });
+
+            //app.UseSwaggerUI(swaggerUIOptions =>
+            //{
+            //    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Tesla API v1");
+            //    swaggerUIOptions.RoutePrefix = string.Empty;
+            //});
 
             app.UseHttpsRedirection();
 
@@ -61,6 +105,9 @@ namespace demoForRouting31
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllerRoute("api", "api/{controller}/{action}");
+
+                // 启用RouteAttribute
                 endpoints.MapControllers();
             });
         }
